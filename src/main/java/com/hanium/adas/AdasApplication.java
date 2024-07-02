fe7676610525c66c5f06047c8005048080af092b
Songhee120 <81075657+Songhee120@users.noreply.github.com>
2024-07-02 14:37:52 +0900

    프로젝트 생성

---
+++ src/main/java/com/hanium/adas/AdasApplication.java
@@ -0,0 +1,13 @@
+package com.hanium.adas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hanium/adas/AdasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hanium.adas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

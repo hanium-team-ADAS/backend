53a70fcfaf7442a6590e44c8f14b5220318372fa
12hyo12 <happyanniean@gmail.com>
2024-08-07 16:41:49 +0900

    singin/signup

---
+++ src/main/java/com/hanium/adas/domain/doctor/dto/SignUpDto.java
@@ -0,0 +1,30 @@
+package com.hanium.adas.domain.doctor.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+
+@Data
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class SignUpDto {
+    @NotNull
+    private String name;
+
+    @NotNull
+    @Email
+    private String email;
+
+    @NotNull
+    private String password;
+
+    @NotNull
+    private String specialization;
+
+    @NotNull
+    private String phone;
+
+    @NotNull
+    private String licenseNumber;
+}

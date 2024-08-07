cfdd5af4f4bd902dbbb546d83ee5302b16a67004
Songhee120 <81075657+Songhee120@users.noreply.github.com>
2024-08-07 17:00:11 +0900

    feat: 체온측정 (MQTT 프로토콜, 모델 API 호출)

--- src/main/java/com/hanium/adas/domain/doctor/api/AuthController.java
+++ src/main/java/com/hanium/adas/domain/doctor/api/AuthController.java
@@ -4,6 +4,7 @@ import com.hanium.adas.domain.doctor.dto.SignInDto;
 import io.swagger.v3.oas.annotations.Operation;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@Slf4j
 @RequestMapping("/doctor")
 public class AuthController {
 
@@ -20,6 +22,7 @@ public class AuthController {
     @Operation(summary = "🟡")
     @PostMapping("/sign-in")
     public ResponseEntity<Boolean> signIn(@RequestBody SignInDto signInDto, HttpServletResponse response) {
+
         return ResponseEntity.ok(authService.signIn(signInDto));
     }
 }

77f1903c3e244f0520225bec37894c38985cf4ab
Songhee120 <81075657+Songhee120@users.noreply.github.com>
2024-08-12 17:42:04 +0900

    update: 중복파일 삭제 및 클래스 이름 변경

--- src/main/java/com/hanium/adas/domain/doctor/api/DoctorAuthController.java
+++ src/main/java/com/hanium/adas/domain/doctor/api/DoctorAuthController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
 @RequestMapping("/doctor")
-public class doctorAuthController {
+public class DoctorAuthController {
 
     private final AuthService authService;
 

37808ebd302bd3fd2811c7e81ae997101d1a57f7
12hyo12 <happyanniean@gmail.com>
2024-08-12 16:12:23 +0900

    feat:doctordetail

---
+++ src/main/java/com/hanium/adas/domain/appointment/api/AppointmentController.java
@@ -0,0 +1,27 @@
+package com.hanium.adas.domain.appointment.api;
+
+import com.hanium.adas.domain.appointment.application.AppointmentService;
+import com.hanium.adas.domain.appointment.dto.DoctorDetailDto;
+import io.swagger.v3.oas.annotations.Operation;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Controller
+@RequestMapping("/appointment")
+public class AppointmentController {
+
+    private final AppointmentService appointmentService;
+
+    @Operation(summary = "🟡")
+    @GetMapping("/doctors")
+    public ResponseEntity<List<DoctorDetailDto>> getDoctorDetails() {
+        List<DoctorDetailDto> doctorDetails = appointmentService.getDoctorDetails();
+        return ResponseEntity.ok(doctorDetails);
+    }
+}

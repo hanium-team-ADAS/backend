ff2fce1c7c096d174835277df36ae93acb08abc8
12hyo12 <happyanniean@gmail.com>
2024-08-14 17:17:58 +0900

    feat:appointmentrequest(Create)

--- src/main/java/com/hanium/adas/domain/appointment/dto/AppointmentRequestDto.java
+++ src/main/java/com/hanium/adas/domain/appointment/dto/AppointmentRequestDto.java
@@ -1,8 +1,14 @@
 package com.hanium.adas.domain.appointment.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class AppointmentRequestDto {
     private Long doctorId;
     private String date;
